e8987f5bf3aefe0b4261860c0d47e6d4d3a8a104
Mohamed Fadl <Mohamed.Fadl2852@gmail.com>
2023-03-05 22:00:04 +0200

    Solve the 7th assignment 🚀💫

--- assignment-7/src/index.ts
+++ assignment-7/src/index.ts
@@ -1,1 +1,10 @@
 console.log('Hello World! 🌎')
+
+function showMsg(user?: string, age?: (number | string), country?: string) {
+    return `${user}${age}${country}`;
+}
+
+console.log(showMsg());
+console.log(showMsg("Elzero"));
+console.log(showMsg("Elzero", 40));
+console.log(showMsg("Elzero", "40", "Egypt"));
