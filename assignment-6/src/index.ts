e2e1865104165fea40c48790d730e75c8fc11476
Mohamed Fadl <Mohamed.Fadl2852@gmail.com>
2023-03-05 21:54:21 +0200

    Solve the 6th assignment 🚀💫

--- assignment-6/src/index.ts
+++ assignment-6/src/index.ts
@@ -1,1 +1,10 @@
 console.log('Hello World! 🌎')
+
+let nothing;
+let theName: string = "ElZero";
+function showMyDetails(a = "", b = "", c?) {
+    return `${a}${b}${c}`;
+}
+
+// Replace ???? With The Available Variables As Argument To Get The Result
+console.log(showMyDetails(theName, nothing)); // ElZero
