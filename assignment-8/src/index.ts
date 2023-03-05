07e0b9f7365055ae281ba7fa5afc49bb757aea83
Mohamed Fadl <Mohamed.Fadl2852@gmail.com>
2023-03-05 23:07:39 +0200

    Solve the 8th assignment 🚀💫

--- assignment-8/src/index.ts
+++ assignment-8/src/index.ts
@@ -1,1 +1,31 @@
 console.log('Hello World! 🌎')
+// Write The Function Here
+const printInConsole = (...args: any[]) : string => {
+    args.forEach((arg) => {
+        return (`The Value Is ${arg} And Type Is ${typeof arg}`);
+    });
+    return ("Done");
+};
+
+// Using The Function => Do Not Edit
+console.log(printInConsole(1, 2, 3, 4, 5));
+console.log(printInConsole("A", "B", "C"));
+console.log(printInConsole(true, false, false, true, true));
+
+// // Output
+// The Value Is 1 And Type Is number
+// The Value Is 2 And Type Is number
+// The Value Is 3 And Type Is number
+// The Value Is 4 And Type Is number
+// The Value Is 5 And Type Is number
+// Done
+// The Value Is A And Type Is string
+// The Value Is B And Type Is string
+// The Value Is C And Type Is string
+// Done
+// The Value Is true And Type Is boolean
+// The Value Is false And Type Is boolean
+// The Value Is false And Type Is boolean
+// The Value Is true And Type Is boolean
+// The Value Is true And Type Is boolean
+// Done
