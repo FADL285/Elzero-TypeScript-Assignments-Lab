dc0f28759cdda01072db8be7d87c6e85b5db2928
Mohamed Fadl <Mohamed.Fadl2852@gmail.com>
2023-03-05 19:44:34 +0200

    Solve the 4th assignment 🚀💫

--- assignment-4/src/index.ts
+++ assignment-4/src/index.ts
@@ -1,1 +1,7 @@
 console.log('Hello World! 🌎')
+let arr: (number | boolean[] | (string | (string | number)[])[])[] = [
+    ["info", ["Mohamed Fadl", 20, "Cairo"]],
+    ["A", "B", "C"],
+    [true, false, true],
+    10, 20, 30,
+]
