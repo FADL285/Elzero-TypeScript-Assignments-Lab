e2507364b7c2f4e5ce2e8d28504b4fab0c73aa97
Mohamed Fadl <Mohamed.Fadl2852@gmail.com>
2023-03-05 19:52:03 +0200

    Solve the 5th assignment 🚀💫

--- assignment-5/src/index.ts
+++ assignment-5/src/index.ts
@@ -1,1 +1,19 @@
 console.log('Hello World! 🌎')
+
+// Update the tsconfig to display the errors the following errors 🚀
+
+// ------------> Unfixed Function
+/*
+function reportErrors(username, age: number) {
+    let rank = "Professor";
+    return `Username: ${username}`;
+    console.log("We Will Not Reach Here");
+}
+*/
+
+// -----------> Fixed the errors in the function
+function reportErrors(username: string) {
+    return `Username: ${username}`;
+}
+
+console.log(reportErrors("FADL"));
