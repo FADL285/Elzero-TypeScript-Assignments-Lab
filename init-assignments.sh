849cfdb55338c8dc49ef5e45744bec029afbedde
Mohamed Fadl <Mohamed.Fadl2852@gmail.com>
2023-03-05 19:20:36 +0200

    Update the bash script to create the index.ts in src and change the rootDir & outDir in tsconfig.ts ️ 🎇🚀🚀💫

--- init-assignments.sh
+++ init-assignments.sh
@@ -10,13 +10,13 @@ fi
 
 read -p "Enter the range of assignments (e.g. 1-6): " assignments_range
 
-read -p "Create src folder? (y/n): " create_src
+read -p "Create src folder? (y/n) - default is true : " create_src
 
 # Check if create_src is y or yes (case insensitive) and set create_src to true else set it to false
-if [[ "$create_src" =~ ^[Yy]$ ]]; then
-  create_src=true
-else
+if [[ "$create_src" =~ ^[Nn]$ ]]; then
   create_src=false
+else
+  create_src=true # Default value
 fi
 
 start=$(echo "$assignments_range" | cut -d '-' -f 1)
@@ -26,12 +26,18 @@ for i in $(seq "$start" "$end"); do
   folder="${folder_name}-$i"
   mkdir -p "$folder"
   cd "$folder" || exit
-  echo "Initializing $folder..."
+  echo "-------------- 🚀 Initializing $folder... 🏗️⚙️⚙ 🚀 ---------------"
   pnpm init
   pnpm add -D typescript
   pnpm tsc --init
   if [ "$create_src" = true ]; then
     mkdir src
+    touch src/index.ts
+    echo "console.log('Hello World! 🌎')" > src/index.ts
+    sed -i 's|// "rootDir": "./"|"rootDir": "./src"|' tsconfig.json
+    sed -i 's|// "outDir": "./"|"outDir": "./dist"|' tsconfig.json
+    sed -i 's#// "noImplicitAny": true,#"noImplicitAny": true,#' tsconfig.json
   fi
+  echo "-------------- 🚀 Initializing the $folder is done 💫✅ 🚀 ---------------"
   cd ..
 done
