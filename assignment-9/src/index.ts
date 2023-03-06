5ceabfb99e2e4f8d040843061868e6c9df8d1e69
Mohamed Fadl <Mohamed.Fadl2852@gmail.com>
2023-03-06 12:22:15 +0200

    Solve the 9th assignment - ( 1st in Data Types assignments ) 🚀💫

--- assignment-9/src/index.ts
+++ assignment-9/src/index.ts
@@ -1,1 +1,8 @@
 console.log('Hello World! 🌎')
+
+// Write Your Code Here
+type n = number;
+// Do Not Edit Here
+let myData: n;
+myData = 1000; // No Problem Here
+myData = +true; // No Problem Here
