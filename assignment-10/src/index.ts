0ebe22231c3c746ce9e5b48fbf95c9d0c4088c7b
Mohamed Fadl <Mohamed.Fadl2852@gmail.com>
2023-03-06 12:23:10 +0200

    Solve the 10th assignment - ( 2nd in Data Types assignments ) 🚀💫

--- assignment-10/src/index.ts
+++ assignment-10/src/index.ts
@@ -1,1 +1,7 @@
 console.log('Hello World! 🌎')
+// Write Your Code Here
+type mix = number | boolean;
+// Do Not Edit Here
+let myInfo: mix;
+myInfo = 1000; // No Problem Here
+myInfo = true; // No Problem Here
