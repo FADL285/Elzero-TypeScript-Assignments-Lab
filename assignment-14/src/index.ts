fbe273f763e20ba2a8f79749be6265382be829b0
Mohamed Fadl <Mohamed.Fadl2852@gmail.com>
2023-03-06 12:53:30 +0200

    Solve the 14th assignment - ( 6th in Data Types assignments ) 🚀💫

--- assignment-14/src/index.ts
+++ assignment-14/src/index.ts
@@ -1,1 +1,17 @@
 console.log('Hello World! 🌎')
+
+// crete a readonly tuple
+let post: readonly [number, string, boolean];
+
+// post = [100, 200, "Title"]; // Error ❌
+// post = ["Title", 100, true]; // Error ❌
+post = [100, "Title", true]; // Good
+
+// post.push("ElZero"); // Error => Cant Add - Readonly 🔒
+
+// Create Destructuring Here
+const [id, title, state] = post;
+// Do Not Edit Here
+console.log(id); // 100
+console.log(title); // "Title"
+console.log(state); // true
