edc621a5156853cde32f9e906c5cd15498be23c3
Mohamed Fadl <Mohamed.Fadl2852@gmail.com>
2023-03-06 12:43:25 +0200

    Solve the 12th & 13th assignment - ( 4th & 5th in Data Types assignments ) 🚀💫

--- assignment-12/src/index.ts
+++ assignment-12/src/index.ts
@@ -1,1 +1,11 @@
 console.log('Hello World! 🌎')
+
+function yesOrNo(val: any ) : "Yes" | "No" | "Error" {
+    if (typeof val !== "number") return "Error";
+    return val > 10 ? "Yes" : "No";
+}
+
+// Do Not Edit Here
+console.log(yesOrNo("100")); // Error
+console.log(yesOrNo(30)); // Yes
+console.log(yesOrNo(8)); // No
--- assignment-13/src/index.ts
+++ assignment-13/src/index.ts
@@ -1,1 +1,12 @@
 console.log('Hello World! 🌎')
+
+type custom = "Yes" | "No" | "Error";
+function isHeOld(age: any) : custom  {
+    if (typeof age !== "number") return "Error";
+    return age > 40 ? "Yes" : "No";
+}
+
+// Do Not Edit Here
+console.log(isHeOld("100")); // Error
+console.log(isHeOld(45)); // "Yes"
+console.log(isHeOld(30)); // "No"
