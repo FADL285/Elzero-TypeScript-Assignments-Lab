e95ce01f312f43e29eafff364ce1c41afe85bf45
Mohamed Fadl <Mohamed.Fadl2852@gmail.com>
2023-03-06 12:25:20 +0200

    Solve the 11th assignment - ( 3rd in Data Types assignments ) 🚀💫

--- assignment-11/src/index.ts
+++ assignment-11/src/index.ts
@@ -1,1 +1,22 @@
 console.log('Hello World! 🌎')
+// Write Your Code Here
+type Info = {
+    theName: string;
+    theAge: number;
+}
+type Full = Info & {
+    country: string;
+}
+// Do Not Edit Here
+function showInfo(data: Info) {
+    console.log(`The Name Is ${data.theName}`);
+    console.log(`The Age Is ${data.theAge}`);
+}
+showInfo({ theName: "Elzero", theAge: 40 });
+
+function showFullInfo(data: Full) {
+    console.log(`The Name Is ${data.theName}`);
+    console.log(`The Age Is ${data.theAge}`);
+    console.log(`The Country Is ${data.country}`);
+}
+showFullInfo({ theName: "Elzero", theAge: 4, country: "Egypt" });
