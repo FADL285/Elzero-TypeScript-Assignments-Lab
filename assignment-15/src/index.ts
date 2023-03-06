24972925eba7809783964bb54536874dd50b0fbb
Mohamed Fadl <Mohamed.Fadl2852@gmail.com>
2023-03-06 13:15:56 +0200

    Solve the 15th assignment - ( 7th in Data Types assignments ) 🚀💫

--- assignment-15/src/index.ts
+++ assignment-15/src/index.ts
@@ -1,1 +1,18 @@
 console.log('Hello World! 🌎')
+
+// Create Enums + Function Here
+function setGameLevel(num: number): number {
+    return Game.Hard - num;
+}
+enum Game {
+    Easy = 100,
+    Medium = Easy - 20,
+    Hard = Medium - (Easy / 2),
+    Insane = setGameLevel(10)
+}
+
+// Output
+console.log(Game.Easy); // 100
+console.log(Game.Medium); // 80
+console.log(Game.Hard); // 30
+console.log(Game.Insane); // 20
