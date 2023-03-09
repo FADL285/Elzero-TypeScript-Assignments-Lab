663cda64828b011c6f751d421e31f1cd79310a66
Mohamed Fadl <Mohamed.Fadl2852@gmail.com>
2023-03-09 21:31:10 +0200

    Solve the Last Assignment in the course 🎇🚀💫

--- assignment-26/src/index.ts
+++ assignment-26/src/index.ts
@@ -1,1 +1,21 @@
-console.log('Hello World! 🌎')
+// Write Class Code Here
+
+class Game<T> {
+    constructor(public title: T, public price: number) {}
+
+    getDiscount(discount: number | string): string {
+        return `The Discount Is ${discount}`;
+    }
+}
+
+// Do Not Edit Here
+let gameOne = new Game<string>("Ys", 100);
+let gameTwo = new Game<number>(2064, 100); // There's A Game Called "2064"
+
+console.log(gameOne.title); // "Ys"
+console.log(gameOne.price); // 100
+gameOne.getDiscount("50"); // "The Discount Is 50"
+
+console.log(gameTwo.title); // 2064
+console.log(gameTwo.price); // 100
+gameTwo.getDiscount(80); // "The Discount Is 80"
