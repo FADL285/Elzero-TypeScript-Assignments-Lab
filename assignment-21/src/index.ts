cb9be3b5e20153f8951aab2798dd7f86de47b3f3
Mohamed Fadl <Mohamed.Fadl2852@gmail.com>
2023-03-09 15:49:27 +0200

    Solve the 21st assignment - ( 5th in Interfaces & Class Part 1 ) 🚀💫

--- assignment-21/src/index.ts
+++ assignment-21/src/index.ts
@@ -1,1 +1,10 @@
 console.log('Hello World! 🌎')
+
+class Shorten {
+    constructor(public id: number,public username: string, protected title: string) {}
+}
+
+let tester = new Shorten(100, "El-zero", "Developer");
+
+console.log(tester.id);
+console.log(tester.username);
