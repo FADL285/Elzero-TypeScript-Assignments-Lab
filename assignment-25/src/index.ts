ffcf55677e8cac3535e71de1b7b517c5d847da91
Mohamed Fadl <Mohamed.Fadl2852@gmail.com>
2023-03-09 21:26:45 +0200

    Solve the 25th assignment - ( 2nd in Class Part 2 And Generics ) 🚀💫

--- assignment-25/src/index.ts
+++ assignment-25/src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello World! 🌎')
+// Write Function Code Here
+function showTypes<T = string, U = string, V = string>(arg1: T = "Nothing" as T, arg2: U = "Nothing" as U, arg3: V = "Nothing" as V): string {
+  return `${arg1} - ${arg2} - ${arg3}`;
+}
+
+// Do Not Edit Here
+console.log(showTypes()); // Nothing - Nothing - Nothing
+console.log(showTypes<string>("String")); // String - Nothing - Nothing
+console.log(showTypes<string, number>("String", 100)); // String - 100 - Nothing
+console.log(showTypes<string, number, boolean>("String", 100, true)); // String - 100 - true
