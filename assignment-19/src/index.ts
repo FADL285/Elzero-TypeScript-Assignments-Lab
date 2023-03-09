93dbb79b1f0445b6dcbf0dc68b64e6643b4333f6
Mohamed Fadl <Mohamed.Fadl2852@gmail.com>
2023-03-09 14:53:45 +0200

    Solve the 19th assignment - ( 3rd in Interfaces & Class Part 1 ) 🚀💫

--- assignment-19/src/index.ts
+++ assignment-19/src/index.ts
@@ -1,1 +1,24 @@
-console.log('Hello World! 🌎')
+// Do Not Edit The Code Below
+interface Man {
+    title: string;
+    weight: number;
+    age: number;
+}
+
+interface Bird {
+    canFly: boolean;
+}
+
+interface Superman extends Man, Bird{
+    bodyType: string;
+    origin: string;
+}
+
+let creature: Superman = {
+    title: "Superman",
+    weight: 100,
+    age: 500,
+    canFly: true,
+    bodyType: "Iron",
+    origin: "Krypton"
+}
