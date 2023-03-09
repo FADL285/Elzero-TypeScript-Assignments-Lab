29f1735092d884d26a4d9016a2a611671cf99378
Mohamed Fadl <Mohamed.Fadl2852@gmail.com>
2023-03-09 15:52:18 +0200

    Solve the 22nd assignment - ( 6th in Interfaces & Class Part 1 ) 🚀💫

--- assignment-22/src/index.ts
+++ assignment-22/src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello World! 🌎')
+class Show {
+    get title(): string {
+        return this._title;
+    }
+
+    set title(value: string) {
+        this._title = value;
+    }
+    constructor (private _title: string) {}
+}
+
+let tester = new Show("FADL");
+
+console.log(tester.title);
+tester.title = "Mohamed Fadl";
+console.log(tester.title);
