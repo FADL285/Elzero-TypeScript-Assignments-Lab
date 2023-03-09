59f394be88543fae80022fe0e5f718ffd0f0be41
Mohamed Fadl <Mohamed.Fadl2852@gmail.com>
2023-03-09 15:56:07 +0200

    Solve the 23rd assignment - ( 7th and last one in Interfaces & Class Part 1 ) 🚀💫

--- assignment-23/src/index.ts
+++ assignment-23/src/index.ts
@@ -1,1 +1,30 @@
-console.log('Hello World! 🌎')
+interface Play {
+    id: number;
+    title: string;
+    level: number | string;
+    logIn(): void;
+    logOut(msg: string): void;
+}
+
+// Create Your Class Here
+
+class Player implements Play {
+    constructor(public id: number, public title: string, public level: number | string) {}
+
+    logIn(): void {
+        console.log("Logged In");
+    }
+
+    logOut(msg: string): void {
+        console.log("Logged Out, " + msg);
+    }
+
+}
+
+let player1 = new Player(100, "Fadl", 95);
+
+console.log(player1.id); // 100
+console.log(player1.title); // "Fadl"
+console.log(player1.level); // 95
+player1.logIn(); // Logged In
+player1.logOut("Goodbye"); // Logged Out, Goodbye
